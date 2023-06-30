8ee6a3d17d4efa33393bcf0369e09db9e89a8d56
JLun-C <137815587+JLun-C@users.noreply.github.com>
2023-06-30 19:58:02 +0800

    Update Player.h

--- Hackathon 2/Hackathon 2/Player.h
+++ Hackathon 2/Hackathon 2/Player.h
@@ -9,30 +9,35 @@ class Player{
 		double Health;			// record the player's current HP
 		double MAX_Health;		// player's starting HP, can be increased
 		double ShieldDura;		// Shield durability. A shield can help to reduce the
-								// incoming damage from enemies and bosses.
+						// incoming damage from enemies and bosses.
 		double DamageTaken;		// the actual damage taken from attacks
 		bool Concentrated;		// status of player affected by his own move
 		
 	public:
 		Player();
-		void Attacked(double, Dice&);	// function to calculate actual damage taken and 
-									// changes in HP
+		void loadPlayerInfo(double, double, double);
+		void Attacked(double, double);		// function to calculate actual damage taken and 
+							// changes in HP
 
 		void Cursed();				// function to update HP when cursed
 		void Heal();
 		void HealHalf();
 		void plusMAXHealth();
-		void repairShield();
 		bool isDead();
+
+		void repairShield();
 		bool hasShield();
+
 		void setConcentrated(bool);
 		bool getConcentrated();
+
 		double getHealth();
 		double getMAX_Health();
 		double getShieldDura();
 		double getDamageTaken();
-		void displayHP();
 
+		void displayHP();
+		void displayPlayerInfo();
 }; 
 
 #endif
